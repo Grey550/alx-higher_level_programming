c9c691e94df4036d3a14145a63ae9a1e66471ad8
Angie <angie.monnye@gmail.com>
2023-04-19 22:01:09 +0200

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,14 +3,17 @@
 
 
 class Base:
+    """A function definition with an ID as an argument"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Function definition with one argument called ID"""
+        """New instance of base
 
+        Args:
+            id: int value
+        """
         if id is None:
             id = Base.__nb_objects + 1
             Base.__nb_objects += 1
-
         self.id = id
