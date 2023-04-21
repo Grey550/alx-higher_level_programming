34ec144103258b49fdb36b696a4b82ba8d242149
Angie <angie.monnye@gmail.com>
2023-04-21 16:05:27 +0200

    First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -35,7 +35,7 @@ class Rectangle(Base):
     @property
     def height(self):
         """height getter"""
-        return self.__width
+        return self.__height
 
     @height.setter
     def height(self, value):
