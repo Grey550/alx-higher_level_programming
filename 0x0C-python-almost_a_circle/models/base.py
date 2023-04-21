662a47e3ee45d8e307868c698881c72156537a59
Angie <angie.monnye@gmail.com>
2023-04-21 21:48:32 +0200

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """Class called base"""
+import json
+import csv
+import turtle
 
 
 class Base:
@@ -17,3 +20,10 @@ class Base:
             id = Base.__nb_objects + 1
             Base.__nb_objects += 1
         self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns a string representation of list dictionaries"""
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
+        return json.dumps(list_dictionaries)
