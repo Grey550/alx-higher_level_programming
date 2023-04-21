81a1c3631dacca761acb8acf3effd133a311c778
Angie <angie.monnye@gmail.com>
2023-04-21 23:34:52 +0200

    File to instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -54,3 +54,15 @@ class Base:
             x = cls(1, 1) if cls.__name__ == "Rectangle" else cls(1)
             x.update(**dictionary)
             return x
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        filename = f"{cls.__name__}.json"
+
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
