6e69fd465aaf8a1b562ae04e5e0d1f615aa30472
Angie <angie.monnye@gmail.com>
2023-06-14 01:24:19 +0200

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,4 +3,4 @@ const firstLine = 'C is fun\n';
 const secondLine = 'Python is cool\n';
 const thirdLine = 'JavaScript is amazing';
 
-console.log(firstLine, secondLine, thirdLine);
+console.log(firstLine + secondLine + thirdLine);
