a2fa6d90cf40e9490229615a7f236c03b565e8a3
Angie <angie.monnye@gmail.com>
2023-06-18 23:49:27 +0200

    Filter by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".
                 format(sys.argv[4]))
     rows = cur.fetchall()
     for r in rows:
