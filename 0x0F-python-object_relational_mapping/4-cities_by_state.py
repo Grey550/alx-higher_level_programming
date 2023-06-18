80a0b9af48ac947a9266e91bbee3d83ba52e810b
Angie <angie.monnye@gmail.com>
2023-06-19 00:35:16 +0200

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa
-where name matches the argument
+script that lists all cities from the database
 """
 import MySQLdb
 import sys
@@ -13,9 +11,9 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
     cur = db.cursor()
-    name = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-                (name, ))
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities JOIN states ON states.id = cities.states_id
+                ORDER BY cities.id ASC""")
     rows = cur.fetchall()
     for r in rows:
         print(r)
